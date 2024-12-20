df60ed288ab693a182e00141196a72563ae42200
Kareem Al Deeb <157403634+Kix-85@users.noreply.github.com>
2024-12-20 18:34:09 +0200

    solving problems

--- UCC/Properties/Resources.Designer.cs
+++ UCC/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProjectX.Properties {
+namespace UCC.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ProjectX.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProjectX.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UCC.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
